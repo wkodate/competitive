6c5481e821ef87f6047742449f0f950f0e70a083
wkodate <hbwandeow@gmail.com>
2019-01-18 01:21:35 +0900

    fix wrong answer

--- atcoder/src/main/java/com/wkodate/atcoder/abc053/a/Main.java
+++ atcoder/src/main/java/com/wkodate/atcoder/abc053/a/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        if (x > 1200) {
+        if (x >= 1200) {
             System.out.println("ARC");
             return;
         }
