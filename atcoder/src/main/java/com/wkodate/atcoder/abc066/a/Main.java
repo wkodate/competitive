5b3f0b75e12cd14c029479462f5c07a116121142
wkodate <hbwandeow@gmail.com>
2019-01-25 00:21:35 +0900

    solve abc066

---
+++ atcoder/src/main/java/com/wkodate/atcoder/abc066/a/Main.java
@@ -0,0 +1,18 @@
+package com.wkodate.atcoder.abc066.a;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int[] array = {a, b, c};
+        Arrays.sort(array);
+        System.out.println(array[0] + array[1]);
+    }
+
+}
