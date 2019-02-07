63b95af8c503abbe3967ffcc1bf53e47fb5482a8
wkodate <hbwandeow@gmail.com>
2019-02-08 01:34:54 +0900

    solve easy problems

---
+++ atcoder/src/main/java/com/wkodate/atcoder/abc074/a/Main.java
@@ -0,0 +1,14 @@
+package com.wkodate.atcoder.abc074.a;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        System.out.println((int) Math.pow(n, 2) - a);
+    }
+
+}
