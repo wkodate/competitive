ba6e8835f197c3e10dfc5e5bf2b26d4bac2d7e84
wkodate <hbwandeow@gmail.com>
2019-02-15 23:16:52 +0900

    solve abc093 and abc094

---
+++ atcoder/src/main/java/com/wkodate/atcoder/abc094/a/Main.java
@@ -0,0 +1,19 @@
+package com.wkodate.atcoder.abc094.a;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int x = sc.nextInt();
+        if (a <= x && a + b >= x) {
+            System.out.println("YES");
+            return;
+        }
+        System.out.println("NO");
+    }
+
+}
