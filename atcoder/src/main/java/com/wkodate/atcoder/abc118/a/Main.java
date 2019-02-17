7b78a7a540beb2242412ba57cf40ef923d09f2fb
wkodate <hbwandeow@gmail.com>
2019-02-17 23:27:24 +0900

    attend abc118

---
+++ atcoder/src/main/java/com/wkodate/atcoder/abc118/a/Main.java
@@ -0,0 +1,18 @@
+package com.wkodate.atcoder.abc118.a;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (b % a == 0) {
+            System.out.println(a + b);
+            return;
+        }
+        System.out.println(b - a);
+    }
+
+}
