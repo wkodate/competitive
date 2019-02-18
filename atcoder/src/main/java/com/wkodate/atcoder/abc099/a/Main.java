43adb66d632043dedd61a57ecbf5f36fba47f936
wkodate <hbwandeow@gmail.com>
2019-02-19 01:43:01 +0900

    solve abc098 and abc099

---
+++ atcoder/src/main/java/com/wkodate/atcoder/abc099/a/Main.java
@@ -0,0 +1,16 @@
+package com.wkodate.atcoder.abc099.a;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.nextInt() <= 999) {
+            System.out.println("ABC");
+            return;
+        }
+        System.out.println("ABD");
+    }
+
+}
