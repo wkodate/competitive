d1b600165f34cd6b064dd8eea9fb022941e1b847
wkodate <hbwandeow@gmail.com>
2019-02-22 00:50:14 +0900

    solve abc100 and abc101

---
+++ atcoder/src/main/java/com/wkodate/atcoder/abc101/a/Main.java
@@ -0,0 +1,21 @@
+package com.wkodate.atcoder.abc101.a;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] s = sc.next().split("");
+        int count = 0;
+        for (int i = 0; i < s.length; i++) {
+            if ("+".equals(s[i])) {
+                count++;
+                continue;
+            }
+            count--;
+        }
+        System.out.println(count);
+    }
+
+}
