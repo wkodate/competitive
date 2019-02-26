08e2ab7386fd9ec85b7c0520af8a303734a1d95d
wkodate <hbwandeow@gmail.com>
2019-02-27 01:56:40 +0900

    solve abc106 and abc107

---
+++ atcoder/src/main/java/com/wkodate/atcoder/abc107/a/Main.java
@@ -0,0 +1,14 @@
+package com.wkodate.atcoder.abc107.a;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = sc.nextInt();
+        System.out.println(n - i + 1);
+    }
+
+}
