3e42d8a06751ede4c95a003c455b5f0b05ba233b
wkodate <hbwandeow@gmail.com>
2019-11-09 13:21:37 +0900

    solve abc020 and abc021

---
+++ atcoder/src/main/java/com/wkodate/atcoder/abc020/a/Main.java
@@ -0,0 +1,17 @@
+package com.wkodate.atcoder.abc020.a;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        if (q == 1) {
+            System.out.println("ABC");
+            return;
+        }
+        System.out.println("chokudai");
+    }
+
+}
