a5b28aecebde1a8e30849dbc1674e808aadfdd1a
wkodate <hbwandeow@gmail.com>
2019-11-20 01:27:22 +0900

    solve nikkei2019_2 contest

---
+++ atcoder/src/main/java/com/wkodate/atcoder/nikkei2019_2/a/Main.java
@@ -0,0 +1,13 @@
+package com.wkodate.atcoder.nikkei2019_2.a;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((n - 1) / 2);
+    }
+
+}
