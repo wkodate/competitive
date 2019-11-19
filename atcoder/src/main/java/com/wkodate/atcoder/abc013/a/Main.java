901b60d3ecc05c9ed1a00078b013f0831aefa818
wkodate <hbwandeow@gmail.com>
2019-11-20 03:59:49 +0900

    solve abc013

---
+++ atcoder/src/main/java/com/wkodate/atcoder/abc013/a/Main.java
@@ -0,0 +1,13 @@
+package com.wkodate.atcoder.abc013.a;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        System.out.println((int) x.toCharArray()[0] - 64);
+    }
+
+}
