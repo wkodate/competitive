d363c338e2d5a4cf4c887706b51bd1f58de60153
wkodate <hbwandeow@gmail.com>
2019-12-17 23:56:06 +0900

    solved day27

---
+++ hackerrank/src/main/java/com/wkodate/hackerrank/thirtydays_of_code/day27/Solution.java
@@ -0,0 +1,42 @@
+package com.wkodate.hackerrank.thirtydays_of_code.day27;
+
+public class Solution {
+
+    public static int minimum_index(int[] seq) {
+        if (seq.length == 0) {
+            throw new IllegalArgumentException("Cannot get the minimum value index from an empty sequence");
+        }
+        int min_idx = 0;
+        for (int i = 1; i < seq.length; ++i) {
+            if (seq[i] < seq[min_idx]) {
+                min_idx = i;
+            }
+        }
+        return min_idx;
+    }
+
+    static class TestDataEmptyArray {
+        public static int[] get_array() {
+            return new int[]{};
+        }
+    }
+
+    static class TestDataUniqueValues {
+        public static int[] get_array() {
+            return;
+        }
+
+        public static int get_expected_result() {
+            return minimum_index();
+        }
+    }
+
+    static class TestDataExactlyTwoDifferentMinimums {
+        public static int[] get_array() {
+        }
+
+        public static int get_expected_result() {
+        }
+    }
+
+}
